4b127da9f926dac1c734308a989ee22d6036ad43
Yevhen Shchukin <eschuk@softserveinc.com>
2023-06-26 17:38:25 +0300

    UC#2 Create list balance endpoint

--- UC2Api/Controllers/BalanceController.cs
+++ UC2Api/Controllers/BalanceController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 using UC2Api.Dto;
 
 namespace UC2Api.Controllers
@@ -9,8 +10,12 @@ namespace UC2Api.Controllers
 	{
 		private readonly HttpClient _httpClient;
 		private readonly string? _apiUrl;
-		private readonly string _balanceTransactionsPath = "balance_transactions";
 		private readonly string? _stripeToken;
+
+		private const string BalanceTransactionsPath = "balance_transactions";
+		private const int MaxLimit = 100;
+		private const int DefaultItemsPerPage = 10;
+		
 
 
 		public BalanceController(HttpClient httpClient, IConfiguration configuration)
@@ -25,7 +30,7 @@ namespace UC2Api.Controllers
 		{
 			try
 			{
-				var callUrl = $"{_apiUrl}/{_balanceTransactionsPath}";
+				var callUrl = $"{_apiUrl}/{BalanceTransactionsPath}";
 
 				_httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {_stripeToken}");
 
@@ -35,6 +40,42 @@ namespace UC2Api.Controllers
 				{
 					var result = await response.Content.ReadFromJsonAsync<BalanceTransactionsResponse>();
 
+					return Ok(result?.Data);
+				}
+				else
+				{
+					return StatusCode((int)response.StatusCode);
+				}
+			}
+			catch
+			{
+				return StatusCode(500);
+			}
+		}
+
+		[HttpGet]
+		[Route("GetPage")]
+		public async Task<IActionResult> GetPage(int? pageNo, int? itemsPerPage)
+		{
+			try
+			{
+				var callUrl = $"{_apiUrl}/{BalanceTransactionsPath}?limit={MaxLimit}";
+
+				_httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {_stripeToken}");
+
+				var response = await _httpClient.GetAsync(callUrl);
+
+				if (response.IsSuccessStatusCode)
+				{
+					var btResponse = await response.Content.ReadFromJsonAsync<BalanceTransactionsResponse>();
+
+					//Pagination
+					var pageNoVal = pageNo ?? 0;
+					var itemsPerPageVal = itemsPerPage ?? DefaultItemsPerPage;
+					var skipCount = itemsPerPageVal * pageNoVal;
+
+					var result = btResponse?.Data?.Skip(skipCount).Take(itemsPerPageVal);
+					
 					return Ok(result);
 				}
 				else
